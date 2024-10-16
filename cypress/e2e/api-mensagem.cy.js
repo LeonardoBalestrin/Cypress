52931e161f1983bd6451cc8f568555276ac95416
Leonardo Balestrin Depelegrin <leobd2506@gmail.com>
2024-10-15 22:23:55 -0300

    adicionando novo arquivo para carregar dados sensiveis uteis em testes, e removendo informações sensiveis do codigo diretamente

--- cypress/e2e/api-mensagem.cy.js
+++ cypress/e2e/api-mensagem.cy.js
@@ -1,12 +1,13 @@
 describe('Página de login', () => {
     //const tempoEsperado = Math.random()*1000
-    const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTcyODk5ODI5MSwiZXhwIjoxNzI5MjU3NDkxfQ.L7qQpFSRQkxNIukNv_EuAeQ02SrjbzgQJfVkiKDBpTo'
-    
+
+
     it('Mensagens da Api',()=>{
         cy.request({
             mothod:"GET",
             url:"https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c",
-            headers:{ authorization }
+            headers:Cypress.env()
+            //passando o token de autorização no arquivo ignorado pelo git "cypress.env.json" podemos usar um token valido sem vazamento de informação sensivel
         }).then((res)=>{
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
